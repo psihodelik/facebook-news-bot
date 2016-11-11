5ffb8874585b1311e99f8555b66581d64be998c8
tomrf1 <tom.forbes@theguardian.com>
2016-11-11 13:52:53 +0000

    Change name of timestamp field in logging

--- src/main/scala/com/gu/facebook_news_bot/utils/Loggers.scala
+++ src/main/scala/com/gu/facebook_news_bot/utils/Loggers.scala
@@ -17,7 +17,7 @@ object Loggers {
 
   def logEvent(json: Json): Unit = {
     //We only write json to event.log, so insert a timestamp here
-    val withTime = json.mapObject(_.add("timestamp", Json.fromString(DateTime.now(DateTimeZone.UTC).toString)))
+    val withTime = json.mapObject(_.add("timestamp_bot", Json.fromString(DateTime.now(DateTimeZone.UTC).toString)))
     eventLogger.info(withTime.noSpaces)
   }
 }
