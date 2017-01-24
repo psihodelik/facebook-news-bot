8ce86f12abcaff02908c4bf94572ded01811d061
Pascal Honoré <pascal@alseyn.net>
2017-01-24 12:11:53 +0000

    Introducing the type UserNoms
    Which given a user ID carries the choices made by the corresponding user.

---
+++ src/main/scala/com/gu/facebook_news_bot/models/UserNoms.scala
@@ -0,0 +1,7 @@
+package com.gu.facebook_news_bot.models
+
+case class UserNoms(ID: String,
+                    bestPicture: Option[String] = None,
+                    bestDirector: Option[String] = None,
+                    bestActor: Option[String] = None,
+                    bestActress: Option[String] = None)
